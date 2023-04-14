3929e657c4d2c8e431a4d3d1edde1fb4458ff901
Joshue2806 <joshue.ruiz@est.ikiam.edu.ec>
2023-04-14 01:56:39 -0500

    Initial commit

---
+++ R/resume.R
@@ -0,0 +1,17 @@
+#' Resumen: Análisis exploratorio de datos
+#'
+#' @param x numeric vector
+#'
+#' @return mena and sd from vector x
+#' @export
+#' @import ggplot2
+#' @import dplyr
+#'
+#' @examples
+#' resumen(c(1, 2, 3, 4, 8))
+#'
+resumen <- function(x){
+  meanx = mean(x, na.rm = TRUE)
+  sdx = sd(x, na.rm = TRUE)
+  print(meanx, sdx)
+}
